16389b259e6d858b7b3da1ab78deb6432e1f91b3
John Murray <john@johnmurray.info>
2015-05-13 12:39:02 +0100

    first commit

---
+++ RoadFix.sql
@@ -0,0 +1,6 @@
+-- Create correction lookup for missing formsPart fields in OS OpenRoads
+CREATE TABLE `roadlink_fix2` 
+SELECT spa_roadlink.identifier,gaz_opennames.OS_ID 
+FROM gaz_opennames 
+INNER JOIN spa_roadlink ON gaz_opennames.NAME1 = spa_roadlink.name1 
+WHERE spa_roadlink.formsPart = '' AND gaz_opennames.OS_ID LIKE 'osgb%' AND ST_INTERSECTS(spa_roadlink.GEOMETRY,gaz_opennames.MBR)
